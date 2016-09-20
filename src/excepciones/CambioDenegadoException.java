0a7a6619303a8d90051c23c3c59b469263ce7e03
Oever González <notengobattery@gmail.com>
2016-09-20 15:51:16 -0600

    Cambia la herencia
    CambioDebegado puede extender fácilmente a OperaciónNoSoportada

--- src/excepciones/CambioDenegadoException.java
+++ src/excepciones/CambioDenegadoException.java
@@ -1,10 +1,12 @@
 package excepciones;
 
+import javax.naming.OperationNotSupportedException;
+
 /**
  * Excepción que se lanza cuando se intenta realizar un cambio que no está permitido ya sea por
  * falta de privilegios o porque el campo se ha bloqueado.
  */
-public class CambioDenegadoException extends Exception {
+public class CambioDenegadoException extends OperationNotSupportedException {
     /**
      * Se lanza cuando se intenta realizar un cambio que no está permitido ya sea por
      * falta de privilegios o porque el campo se ha bloqueado.
